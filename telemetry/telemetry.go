8b8594c1cc0b119358a0b8e55283513166e4918d
Ramesh Kumar <codeofnode@gmail.com>
2020-08-09 17:05:35 +0530

    CODEOFNODE/KEEN2CHART- sending it to plain

--- telemetry/telemetry.go
+++ telemetry/telemetry.go
@@ -47,7 +47,7 @@ type Telemetry struct {
 	NumGraphQL   uint64 `json:",omitempty"`
 }
 
-const url = "https://ping.dgraph.io/3.0/projects/5b809dfac9e77c0001783ad0/events"
+const url = "https://ping.dgraph.io"
 
 // NewZero returns a Telemetry struct that holds information about the state of zero server.
 func NewZero(ms *pb.MembershipState) *Telemetry {
@@ -103,9 +103,6 @@ func (t *Telemetry) Post() error {
 		return err
 	}
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "D0398E8C83BB30F67C519FDA6175975F680921890C35B36C34BE1095445"+
-		"97497CA758881BD7D56CC2355A2F36B4560102CBC3279AC7B27E5391372C36A31167EB0D06BF3764894AD20"+
-		"A0554BAFF14C292A40BC252BB9FF008736A0FD1D44E085")
 
 	client := &http.Client{Timeout: 10 * time.Second}
 	resp, err := client.Do(req)
